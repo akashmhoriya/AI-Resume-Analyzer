c3a7b804aca2f1b29e3c2e45691857157572f442
akashmhoriya <akashmhoriyaofficial@gmail.com>
2026-04-12 23:37:03 +0530

    update upload feature

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,17 +1,27 @@
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
-interface FileUploaderProps{
-  onFileSelect?: (file:File | null)=> void;
+interface FileUploaderProps {
+  onFileSelect?: (file: File | null) => void;
 }
 
-const FileUploader = ({onFileSelect}:FileUploaderProps) => {
-  const [file, setFile] = useState();
-  const onDrop = useCallback((acceptedFiles:File[]) => {
-    // Do something with the files
+const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      // Do something with the files
+      const file = acceptedFiles[0] || null;
+      onFileSelect?.(file);
+    },
+    [onFileSelect],
+  );
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      accept: { "application/pdf": [".pdf"] },
+      maxSize: 20 * 1024 * 1024,
+    });
+
     const file = acceptedFiles[0] || null;
-    onFileSelect?.(file);
-  }, [onFileSelect]);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
     <div className="w-full gradient-border">
       <div {...getRootProps()}>
@@ -22,16 +32,17 @@ const FileUploader = ({onFileSelect}:FileUploaderProps) => {
             <img src="/icons/info.svg" alt="upload" className="size-20" />
           </div>
 
-          {file ? <div>
-
-          </div> : <div>
-            <p className="text-lg text-gray-500">
-              <span className="font-semibold">
-                Click to upload
-              </span> or drag and drop
+          {file ? (
+            <div></div>
+          ) : (
+            <div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
               </p>
               <p className="text-lg text-gray-500">PDF (max 20 MB)</p>
-              </div>}
+            </div>
+          )}
         </div>
       </div>
     </div>
